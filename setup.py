2d1437c8b2fcbed3dfa032362f3737be5d68c057
Tomasz Janicki <tomoicki@gmail.com>
2021-08-20 13:20:12 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='JobOffers_DataExtraction',
+    version='final',
+    packages=['utilities'],
+    url='https://github.com/tomoicki/JobOffers_DataExtraction/tree/using_requests',
+    license='myown',
+    author='nobody',
+    author_email='none',
+    description='test'
+)
