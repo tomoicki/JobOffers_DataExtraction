b276d308aa75730a1485125ce544e6c9adab8f60
Tomasz Janicki <tomoicki@gmail.com>
2021-08-24 11:17:24 +0200

    asd

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-from setuptools import find_namespace_packages
+from setuptools import find_packages
 
 # Load the README file.
 with open(file="README.md", mode="r") as readme_handle:
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/tomoicki/JobOffers_DataExtraction/tree/using_requests',
-    packages=find_namespace_packages(where=['utilities']),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
         'requests == 2.26.0',
         'pandas == 1.3.2',
