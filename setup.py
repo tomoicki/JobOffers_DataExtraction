b7ce448e927d5846456ef1c1e4fc74304e0fb870
Tomasz Janicki <tomoicki@gmail.com>
2021-08-24 11:03:15 +0200

    aaaa

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
+from setuptools import find_namespace_packages
+
+# Load the README file.
+with open(file="README.md", mode="r") as readme_handle:
+    long_description = readme_handle.read()
 
 setup(
     name='JobOffers_DataExtraction',
     version='0.0.1',
-    packages=['utilities'],
+    description='A python client library used to leverage the different libraries provided by Sigma Coding.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tomoicki/JobOffers_DataExtraction/tree/using_requests',
+    packages=find_namespace_packages(where=['utilities']),
     install_requires=[
         'requests == 2.26.0',
         'pandas == 1.3.2',
