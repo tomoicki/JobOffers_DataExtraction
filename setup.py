16cca2879c6ba7cff0a4257032b7242d97737d06
Tomasz Janicki <tomoicki@gmail.com>
2021-08-26 08:22:39 +0200

    just setup

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
-from setuptools import find_packages
 
-# Load the README file.
 with open(file="README.md", mode="r") as readme_handle:
     long_description = readme_handle.read()
 
 setup(
     name='JobOffers_DataExtraction',
     version='0.0.1',
-    description='A python client library used to leverage the different libraries provided by Sigma Coding.',
+    description='Job offers scraper for nofluffjobs.com and justjoin.it.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/tomoicki/JobOffers_DataExtraction/tree/using_requests',
-    packages=find_packages(exclude=["tests"]),
+    packages=["utilities"],
     install_requires=[
         'requests == 2.26.0',
         'pandas == 1.3.2',
